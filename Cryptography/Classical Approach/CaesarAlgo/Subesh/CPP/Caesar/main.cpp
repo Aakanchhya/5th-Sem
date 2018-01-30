f654f87fe6df83ef354000cd54564d1eb9000cad
subeshb1 <subeshb1@gmail.com>
2018-01-30 20:12:49 +0545

    Caesar Subesh

---
+++ Cryptography/Classical Approach/CaesarAlgo/Subesh/CPP/Caesar/main.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+class Caesar {
+public:
+
+    string encrypt(string text,int key) {
+        string encoded = "";
+        for(int i = 0; i < text.length(); i ++ ) {
+            if(text[i] != ' ')
+            encoded += (text[i] - 97 + key) % 26 + 97;
+            else
+                encoded += " ";
+
+        }
+
+        return encoded;
+    }
+    string decrypt(string text,int key) {
+        string decoded = "";
+        for(int i = 0; i < text.length(); i ++ ) {
+            if(text[i] != ' ')
+                decoded += abs((26 + text[i] - 97 - key)) % 26 + 97;
+            else
+                decoded += " ";
+
+        }
+
+        return decoded;
+    }
+
+};
+
+
+int main()
+{
+    int key = 25;
+    Caesar *s = new Caesar;
+    string plaintText = "i love you aakanchhya";
+    string cipherText = s->encrypt(plaintText, key);
+    string decodedText = s->decrypt(cipherText, key);
+    cout << "Plain Text: " << plaintText << endl;
+    cout << "Key: " << key << endl;
+
+    cout << "Encoded Text: " << cipherText << endl;
+    cout << "Decoded Text: " << decodedText << endl;
+
+    return 0;
+}
