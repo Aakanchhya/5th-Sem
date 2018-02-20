74b34ad0117d141d2efc7a6165ae0bbe692becb9
Aakanchhya <aakanchhyas@gmail.com>
2018-02-20 12:28:38 +0545

    Committed to pull by aku

--- Cryptography/ClassicalApproach/Transposition/Rail-fenceAlgo/Aaku/CPP/Rail-fence/main.cpp
+++ Cryptography/ClassicalApproach/Transposition/Rail-fenceAlgo/Aaku/CPP/Rail-fence/main.cpp
@@ -7,8 +7,10 @@ using namespace std;
 class Rail_Fence
 {
 public:
-    string encryption(string,int);
-    string decryption(string,int);
+    string straightEncrypt(string,int);
+    string straightDecrypt(string,int);
+    string daigonalEncrypt(string,int);
+    string diagonalDecrypt(string,int);
     string removeWhiteSpace(string);
 };
 
@@ -26,11 +28,11 @@ string Rail_Fence::removeWhiteSpace(string symbol)
     return sym;
 }
 
-string Rail_Fence::encryption(string sym, int number)
+string Rail_Fence::straightEncrypt(string sym, int number)
 {
     string symbol;
     symbol = removeWhiteSpace(sym);
-    cout << endl << "I am inside Encryption" << endl ;
+    cout << endl << "I am inside straight Encryption" << endl ;
     int num = 0 ;
     float key;
     string cipher = "";
@@ -53,9 +55,9 @@ string Rail_Fence::encryption(string sym, int number)
     return cipher;
 }
 
-string Rail_Fence::decryption(string symbol, int number)
+string Rail_Fence::straightDecrypt(string symbol, int number)
 {
-    cout << endl << "I am inside Decryption" << endl ;
+    cout << endl << "I am inside straight Decryption" << endl ;
     int key;
     key = symbol.length()/number ;
     int num;
@@ -73,13 +75,81 @@ string Rail_Fence::decryption(string symbol, int number)
     return plainText;
 }
 
+string Rail_Fence::daigonalEncrypt(string sym, int number)
+{
+    cout << "I am inside diagonal Encryption" << endl ;
+    string symbol = removeWhiteSpace(sym);
+    string cipher[100];
+    for(int i=0; i<100; i++)
+    {
+        cipher[i] = "";
+    }
+
+    string cipherText = "";
+
+    //Looping to get cipher text
+    int row = 0;
+    int count = 0;
+
+    for(int column=0; column<symbol.length(); column++)
+    {
+        cout << endl << "I am inside i loop: column = " << column << endl ;
+        if(count < number)
+        {
+            cout << endl << "Inside if";
+            cipher[row] += symbol[column];
+            cout << endl << "cipher = " << cipher[row] ;
+            cout << endl << "row = " << row << endl ;
+            row++;
+            count++;
+        }
+        else
+        {
+            if(row == number)
+            {
+                row--;
+            }
+            cout << endl << "Inside else";
+            row--;
+            cipher[row] += symbol[column];
+            cout << endl << "cipher = " << cipher[row] ;
+            cout << endl << "row = " << row << endl;
+            count ++;
+            if(row == 0)
+            {
+                count =1;
+                row = 1;
+            }
+        }
+    }
+
+    cout << row ;
+
+    for(int i=0; i<number; i++)
+    {
+
+    }
+
+    //Concatinating strings
+    for(int i=0; i<number; i++)
+    {
+        cipherText += cipher[i];
+    }
+
+    return cipherText;
+}
+
 int main()
 {
     Rail_Fence R;
     string encryptedCode, decryptedCode;
-    encryptedCode = R.encryption("he is most wanted Criminal",3);
+    encryptedCode = R.straightEncrypt("he is most wanted Criminal",3);
     cout << "Encrypted Code = " << encryptedCode << endl ;
-    decryptedCode = R.decryption(encryptedCode,3);
+    decryptedCode = R.straightDecrypt(encryptedCode,3);
     cout << "Decrypted Code = " << decryptedCode << endl ;
+
+    string encrypt ;
+    encrypt = R.daigonalEncrypt("they will attack on tomorrow",3);
+    cout << endl << encrypt;
     return 0;
 }
